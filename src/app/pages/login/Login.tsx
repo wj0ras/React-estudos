a006b36f83424565ef83afb9f9ccbf234298bd72
Welison Joras <welison.joras@hotmail.com>
2024-04-24 13:11:56 -0300

    useMemo

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    const emailLength = useMemo(() => {
+        console.log('executou')
+        return email.length;
+    }, [email.length]);
 
     const handleClick = () => {
         navigate('/dashboard')
@@ -17,6 +22,8 @@ export const Login = () => {
     return(
         <div>
             <form>
+                <p>Quantidade de caracteres no email: {emailLength}</p>
+
                 <label>
                     <span>Email</span>
                     <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
